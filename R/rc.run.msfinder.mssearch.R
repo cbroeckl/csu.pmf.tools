10d21b12782f8adee6d76e4b494d8c1609ebae62
Corey Broeckling <cbroeckl@users.noreply.github.com>
2022-08-31 09:58:01 -0600

    comment out run.sirius.msfinder

--- R/rc.run.msfinder.mssearch.R
+++ R/rc.run.msfinder.mssearch.R
@@ -1,29 +1,29 @@
-rc.run.sirius <- function(
-  sirius.dir = "/home/pmflab/sirius-gui/bin/sirius",
-  input.dir  =  "/home/pmflab/RStor/Projects/20210701-KWEBB-KW-1044/TOF_T3_Pos/spectra/ms",
-  output.dir = "out"
-) {
-  
-  system(
-    paste0(
-      sirius.dir, " ",
-      "-i=",
-      input.dir, 
-      " -o=",
-      input.dir, "/out",
-      " --recompute ", 
-      "--naming-convention=%compoundname ", 
-      "--cores 24 ",
-      "formula ",
-      "-d ", 
-      "bio ", 
-      "-p ", 
-      "qtof ", 
-      "zodiac ", 
-      "structure ", 
-      "canopus"
-    )
-  )
-  
-}
-
+# rc.run.sirius <- function(
+#   sirius.dir = "/home/pmflab/sirius-gui/bin/sirius",
+#   input.dir  =  "/home/pmflab/RStor/Projects/20210701-KWEBB-KW-1044/TOF_T3_Pos/spectra/ms",
+#   output.dir = "out"
+# ) {
+#   
+#   system(
+#     paste0(
+#       sirius.dir, " ",
+#       "-i=",
+#       input.dir, 
+#       " -o=",
+#       input.dir, "/out",
+#       " --recompute ", 
+#       "--naming-convention=%compoundname ", 
+#       "--cores 24 ",
+#       "formula ",
+#       "-d ", 
+#       "bio ", 
+#       "-p ", 
+#       "qtof ", 
+#       "zodiac ", 
+#       "structure ", 
+#       "canopus"
+#     )
+#   )
+#   
+# }
+# 
--- R/sirius.command.line.R
+++ R/sirius.command.line.R
@@ -1,22 +1,22 @@
-system(
-  paste0(
-    "/home/pmflab/sirius-gui/bin/sirius ", 
-    "-i=",
-    "/home/pmflab/RStor/Projects/20210701-KWEBB-KW-1044/TOF_T3_Pos/spectra/ms", 
-    " -o=",
-    "/home/pmflab/RStor/Projects/20210701-KWEBB-KW-1044/TOF_T3_Pos/spectra/ms/out",
-    " --recompute ", 
-    "--naming-convention=%compoundname ", 
-    "--cores 24 ",
-    # "--compound-timeout=20 ", 
-    "formula ",
-    "-d ", 
-    "bio ", 
-    "-p ", 
-    "qtof ", 
-    "zodiac ", 
-    "structure ", 
-    "canopus"
-  )
-)
-
+# system(
+#   paste0(
+#     "/home/pmflab/sirius-gui/bin/sirius ", 
+#     "-i=",
+#     "/home/pmflab/RStor/Projects/20210701-KWEBB-KW-1044/TOF_T3_Pos/spectra/ms", 
+#     " -o=",
+#     "/home/pmflab/RStor/Projects/20210701-KWEBB-KW-1044/TOF_T3_Pos/spectra/ms/out",
+#     " --recompute ", 
+#     "--naming-convention=%compoundname ", 
+#     "--cores 24 ",
+#     # "--compound-timeout=20 ", 
+#     "formula ",
+#     "-d ", 
+#     "bio ", 
+#     "-p ", 
+#     "qtof ", 
+#     "zodiac ", 
+#     "structure ", 
+#     "canopus"
+#   )
+# )
+# 
